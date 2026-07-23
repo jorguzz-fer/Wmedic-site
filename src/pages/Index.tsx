f3f0437b96a1b3e7db7d5662b74283777ee97491
jorguzz-fer <fer.jorge@gmail.com>
2026-07-23 18:05:04 -0300

    feat(home): compose WMedic Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/Header";
 import HeroSlider from "@/components/HeroSlider";
-import ServicesSection from "@/components/ServicesSection";
 import AboutSection from "@/components/AboutSection";
-import NetworkSection from "@/components/NetworkSection";
+import ServicesSection from "@/components/ServicesSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import EspecialidadesSection from "@/components/EspecialidadesSection";
-import FAQSection from "@/components/FAQSection";
+import NetworkSection from "@/components/NetworkSection";
 import ComparativoSection from "@/components/ComparativoSection";
+import FAQSection from "@/components/FAQSection";
 import MarqueeSection from "@/components/MarqueeSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
@@ -18,11 +18,11 @@ const Index = () => {
       <HeroSlider />
       <AboutSection />
       <ServicesSection />
-      <NetworkSection />
       <FeaturesSection />
       <EspecialidadesSection />
-      <FAQSection />
+      <NetworkSection />
       <ComparativoSection />
+      <FAQSection />
       <MarqueeSection />
       <CTASection />
       <Footer />
